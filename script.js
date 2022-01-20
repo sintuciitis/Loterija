a8e6ef807da39c7cbbe6047b96c510b9e17fe021
sintuciitis <sintijako222@gmail.com>
2022-01-20 15:04:05 +0200

    2

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert(1);
+const vardi = ['Elza Krūze', 'Sintija Liepa', 'Anete Ozola', 'Mārtiņš Lapsa', 'Kitija Kociņa']  //const - masīvi(nemainīgs)
+const balvas = ['Saulesbrilles', 'Dators', 'Ceļojums uz Itāliju', 'Velosipēds', 'Friziera apmeklējums']
+const naudaKopa = 1000000;  
+
+let uzvaretajuSkaits = 5;  //let - mainīgie, kuri attiecas uz kādu vienu sadaļu
+
+let rand = Math.random()*5;  //nejauši izvēlēsies 5 uzvarētājus
+rand = Math.floor(rand);  //nozīmē, ka apaļo uz leju, lai neivēlētos 3,7 , bet 4
+console.log(vardi[rand]);  //izvada datus konsolē
