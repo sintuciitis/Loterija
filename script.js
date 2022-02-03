cc9d1e09f56e0873ba2781d97fe36218717a470f
sintuciitis <sintijako222@gmail.com>
2022-02-03 15:17:59 +0200

    Puse

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ const naudaKopa = 1000000;
 
 let uzvaretajuSkaits = 5;  //let - mainīgie, kuri attiecas uz kādu vienu sadaļu
 
-let rand = Math.random()*5;  //nejauši izvēlēsies 5 uzvarētājus
-rand = Math.floor(rand);  //nozīmē, ka apaļo uz leju, lai neivēlētos 3,7 , bet 4
-console.log(vardi[rand]);  //izvada datus konsolē
+let rindas = document.querySelector('.rindas');
+rindas.innerHTML = ''; 
+
+for(let i = 0; i <uzvaretajuSkaits; i++){
+    let rand = Math.random()*5;   //nejauši izvēlēsies 5 uzvarētājus
+    rand = Math.floor(rand);  //nozīmē, ka apaļo uz leju, lai neivēlētos 3,7 , bet 4
+
+let uzvaretajs = vardi[rand]  //izvada datus konsolē
+let uzvaretajs1 = balvas[rand]
+
+rindas.innerHTML +=`
+
+<tr>
+<td>${i+1}</td>
+<td>${uzvaretajs}</td>
+<td>${uzvaretajs1}</td>
+</tr>`
+
+}
